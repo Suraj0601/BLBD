2fe6c30917e496648396159de4fa6a25eab3cba6
Suraj0601 <109669478+Suraj0601@users.noreply.github.com>
2025-07-14 15:02:07 +0530

    Update Aboutus.js

--- src/components/Aboutus.js
+++ src/components/Aboutus.js
@@ -16,7 +16,7 @@ export default function Aboutus() {
                 </div>
                 <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s" style={{visibility: 'visible', animationdelay: '0.5s', animationname: 'fadeIn'}}>
                     {/* <p className="d-inline-block border rounded-pill py-1 px-4">About Us</p> */}
-                    <h1 className="mb-4">Why You Should Trust Us? Get Know About Us!</h1>
+                    <h1 className="mb-4">Get Know About Us!</h1>
                     <p>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
                     <p className="mb-4">Stet no et lorem dolor et diam, amet duo ut dolore vero eos. No stet est diam rebum amet diam ipsum. Clita clita labore, dolor duo nonumy clita sit at, sed sit sanctus dolor eos.</p>
                     <p><i className="far fa-check-circle text-primary me-3"></i>Quality health care</p>
